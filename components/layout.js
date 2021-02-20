6bd9959c8a7923c235e0a12e4c3e7d42ed61240e
Thiago Melo <thiago.tmwd@gmail.com>
2021-02-20 16:19:46 -0300

    Change title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Next.js Web site'
 
 export default function Layout({ children, home }) {
   return (
